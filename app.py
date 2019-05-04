281354754154ea7319ac49059d6f3a307b3a7e26
Alexey Orlenko <eaglexrlnk@gmail.com>
2019-05-04 15:54:09 +0300

    Load files from directories

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 import logging
+import os
 
 import cpp_parser
 from tree import ASTPrinter
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARNING)
 
-parser = cpp_parser.Parser()
-parser.parse('example.cc')
 
-for fn, ast in parser.blocks.items():
+def parse_files(root_directory):
+    parser = cpp_parser.Parser()
+
+    for directory, _, filenames in os.walk(root_directory):
+        for filename in filenames:
+            parts = os.path.splitext(filename)
+            if len(parts) > 1 and parts[1] in ('.cc', '.cpp', '.h', 'hpp', '.cxx', '.hxx'):
+                parser.parse(os.path.join(directory, filename))
+
+    return parser
+
+
+known_samples = parse_files('known_samples')
+to_check = parse_files('to_check')
+
+
+for fn, ast in known_samples.blocks.items():
     print(fn)
     ast_printer = ASTPrinter(ast)
     ast_printer.print()
---
+++ to_check/adder.cc
@@ -0,0 +1,6 @@
+class Adder {
+  public:
+    static void add(int x, int y) {
+        return x + y;
+    }
+};
